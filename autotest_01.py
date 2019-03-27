18ebe61bb072a92a8659225670d79759ec315d82
Kirill Kovalchuk <47780190+kirill-kov@users.noreply.github.com>
2019-03-27 10:57:18 +0700

    first autotest

---
+++ autotest_01.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+
+elem = driver.find_element_by_name("q")
+elem.send_keys("Hello WebDriver!")
+elem.submit()
+
+print(driver.title)
